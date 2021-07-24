6186990f63479a8735b3bee3f5fc8af368d194c3
Kunta-M <mariia.kunta@gmail.com>
2021-07-25 00:20:34 +0300

    additional less5

---
+++ lesson5_cw/lesson5_add.js
@@ -0,0 +1,14 @@
+// - Створити форму з інпутом для введення ім'я, та action="index2.html".
+// При відправці данних з форми, зберігати ім'я в localstorage, та переходити на index2.html.
+// На index2.html виводити записане в localstorage ім'я в div
+
+let login = document.forms.login;
+let nameInput = login.name;
+
+login.addEventListener('click', () => {
+    let name = nameInput.value;
+    let user = {name: name};
+
+    let jsonUser = JSON.stringify(user);
+    localStorage.setItem('user', jsonUser);
+});
