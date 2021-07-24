4a420dc2060eea1ac38c03321b07a533221d5461
Kunta-M <mariia.kunta@gmail.com>
2021-07-24 14:39:20 +0300

    cw lesson5 done

--- lesson5_hw/lesson5_hw.js
+++ lesson5_hw/lesson5_hw.js
@@ -4,15 +4,18 @@
 // -- отримує текст з параграфа з id "content"
 
 let content = document.getElementById('content')
+console.log(content.innerText)
 
 // -- отримує текст з блоку з id "rules"
 
 let rules = document.getElementById('rules')
+console.log(rules.innerText);
 
 // -- замініть текст параграфа з id 'content' на будь-який інший
 content.innerText ='будь-який інший'
 
 console.log(content);
+
 // -- замініть текст параграфа з id 'rules' на будь-який інший
 rules.innerHTML = 'ще один будь-який інший'
 
@@ -31,11 +34,15 @@ let rulesList = document.getElementById('rules');
 console.log(rulesList.classList);
 
 // -- отримати всі елементи з класом fc_rules. визначити їм 2 події "клік", одна вивдоить текст елементу, інша довільний ьексь
-let fcList = document.getElementsByClassName('fc_rules')
-for (const fcListElement of fcList) {
-    fcListElement.onclick = function (ev){
-            console.log(innerHTML = 'Hello')
-    }
+let fcRules = document.getElementsByClassName('fc_rules')
+for (const fcRule of fcRules) {
+    fcRule.addEventListener('click', function (){
+        console.log(fcRule.innerHTML)
+    })
+
+    fcRule.addEventListener('click', function (){
+        alert('Hello')
+    })
 }
 
 // -- поміняти колір тексту у всіх елементів fc_rules на червоний
