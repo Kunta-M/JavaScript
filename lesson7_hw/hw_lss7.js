de3d069819f82b6e8fb6b1ed1d486b6cae7c304c
Kunta-M <mariia.kunta@gmail.com>
2021-08-03 00:06:00 +0300

    lss7_cw almost done

--- lesson7_hw/hw_lss7.js
+++ lesson7_hw/hw_lss7.js
@@ -13,84 +13,97 @@
 //
 // І так далі
 
-function wakeUp (coffee) {
-    return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        coffee = 300;
-        if (coffee <= 280){
-            reject ('Need more coffee', null)
-        } else resolve (null, 'Good morning')
-    }, 300)
-    })
-}
-
-function breakfest (food){
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            resolve ('Breakfest done');
-        }, 600)
-    })
-}
-
-function shower (energy) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            resolve ('Ready to go');
-        }, 400)
-    })
-}
-
-function wayToWork (buses){
-    return new Promise ((resolve, reject) =>{
-        if (buses <= 1){
-            resolve ('Good way', null)
-        } else reject (null, 'Bad way')
-    }, 1000)
-}
-
-function work (hours){
-    return new Promise((resolve, reject) => {
-        if (hours <= 7){
-            reject ('Work harder', null)
-        } else resolve (null, 'Good job')
-    }, 800)
-}
-
-async function scheduleDay () {
-    try {
-        const a = await wakeUp(300);
-        console.log('Good morning');
-
-            const b = await breakfest();
-            console.log('Breakfest done');
-
-            const c = await shower();
-            console.log('Ready to go');
-
-            const d = await wayToWork(1);
-            console.log('Good way');
-
-            const e = await work(8)
-            console.log('Good job')
-        } catch (reason) {
-            console.log('Work harder')
-        }
-}
-scheduleDay()
-
-
-
+// function wakeUp (coffee) {
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         coffee = 300;
+//         if (coffee <= 280){
+//             reject ('Need more coffee', null)
+//         } else resolve (null, 'Good morning')
+//     }, 300)
+//     })
+// }
+//
+// function breakfast (food){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             resolve ('Breakfast done');
+//         }, 600)
+//     })
+// }
+//
+// function shower (energy) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             resolve ('Ready to go');
+//         }, 400)
+//     })
+// }
+//
+// function wayToWork (buses){
+//     return new Promise ((resolve, reject) =>{
+//         if (buses > 1){
+//             reject ('Bad way', null)
+//         } else resolve (null,'Good way')
+//     }, 1000)
+// }
+//
+// function work (hours){
+//     return new Promise((resolve, reject) => {
+//         if (hours <= 7){
+//             reject ('Work harder', null)
+//         } else resolve (null, 'Good job')
+//     }, 800)
+// }
+//
+// function lunch (energy){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve ('Full of energy')
+//         }, 500)
+//     })
+// }
+//
+// function goHome (hours){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (hours > 1){
+//                 reject ('Very long', null)
+//             } else resolve (null, 'Not so long')
+//         }, 1000)
+//     })
+// }
+//
+// function dinner (energy){
+//     return new Promise((resolve, reject) => {
+//         setTimeout( () => {
+//             resolve ('Ready to study')
+//         }, 500)
+//     })
+// }
+//
+// function study (hours){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             if (hours <= 4){
+//                 reject ('Study better', null)
+//             } else resolve (null, 'Good result')
+//         }, 1500)
+//     })
+// }
+//
+//
 // wakeUp(320)
-// .then((value) => {
-//     console.log('Good morning');
+//     .then((value) => {
+//         console.log('Good morning');
 //
-//     return breakfest();
-// })
-// .then(value => {
-//     console.log('Breakfest done')
+//         return breakfast();
+//     })
+//     .then(value => {
+//         console.log('Breakfast done')
 //
-//     return shower ();
-// })
+//         return shower ();
+//     })
 //     .then (value => {
 //         console.log('Ready to go')
 //
@@ -99,10 +112,106 @@ scheduleDay()
 //     .then( value => {
 //         console.log('Good way')
 //
-//         return work(7)
+//         return work(8)
+//     })
+//     .then(value => {
+//         console.log('Good job')
+//
+//         return lunch();
+//     })
+//     .then (value => {
+//         console.log('Full of energy')
+//
+//         return goHome(1)
+//     })
+//     .then(value => {
+//         console.log('Not so long')
+//
+//         return dinner();
+//     })
+//     .then(value => {
+//         console.log('Ready to study')
+//
+//         return study(3)
 //     })
 //     .catch(reason => {
-//         console.log('Work harder')
+//         console.log('Study better')
 //     })
+
+
+// async function scheduleDay () {
+//     try {
+//         const a = await wakeUp(100);
+//         console.log('Good morning');
+//
+//         const b = await breakfast();
+//         console.log('Breakfast done');
+//
+//         const c = await shower();
+//         console.log('Ready to go');
+//
+//         const d = await wayToWork(1);
+//         console.log('Good way');
+//
+//         const e = await work(8)
+//         console.log('Good job')
+//
+//         const f = await lunch ();
+//         console.log('Ready to work again')
+//
+//         const g = await goHome( 1);
+//         console.log('Not so long')
+//
+//         const h = await dinner();
+//         console.log('Ready to study')
+//
+//         const i = await study(3)
+//         console.log('Good result')
+//
+//         } catch (reason) {
+//             console.log('Study better')
+//         }
+// }
+// scheduleDay()
+
+
+
+// ***********************  CALLBACK ************************
+
+function wakeUpCB (coffee, cb) {
+    setTimeout(() => {
+        coffee = 300;
+        if (coffee < 300) {
+            console.log('Need more coffee');
+        } else {
+            console.log('Good morning');
+            cb(null, 'Good morning!!!')
+        }
+    }, 300)
+}
+
+function breakfastCB (food, cb){
+        setTimeout(() => {
+            food = 400;
+            console.log('Breakfast done');
+        }, 600)
+}
+
+function showerCB (energy, cb) {
+        setTimeout(() =>{
+            console.log('Ready to go');
+        }, 400)
+}
+
+wakeUpCB(300, () => {
+        breakfastCB( (400), () => {
+        })
+    })
+
+
+
+
+
+
 
 
