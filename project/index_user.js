be2d73b67c6e0da5e27fb1dd31489951b2dafed8
Kunta-M <mariia.kunta@gmail.com>
2021-08-04 23:00:16 +0300

    js project

--- project/index_user.js
+++ project/index_user.js
@@ -3,6 +3,8 @@
 // 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
 // 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
 
+const userTitle = document.getElementById('title');
+userTitle.innerText = 'Users'
 
 const mainUsersDiv = document.getElementsByClassName('users')[0];
 
@@ -11,15 +13,19 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => {
         for (const user of value) {
             console.log(user)
-            const userDiv = document.createElement('div');
+
+            const userDiv = document.createElement('h4');
+
+            const link = document.createElement('div')
             const linkDetails = document.createElement('a');
 
             userDiv.innerText = `${user.id}. ${user.name}`;
 
-            linkDetails.innerText = 'Details'
+            linkDetails.innerText = 'details';
             linkDetails.href = `user_details.html?user=${JSON.stringify(user)}`;
 
-            userDiv.append(linkDetails)
+            link.append(linkDetails);
+            userDiv.append(link);
             mainUsersDiv.append(userDiv)
         }
     })
