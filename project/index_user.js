9f8de4175cf677ecc62e9dd06d4e753a99d94dd2
Kunta-M <mariia.kunta@gmail.com>
2021-08-04 22:00:05 +0300

    js project

--- project/index_user.js
+++ project/index_user.js
@@ -4,7 +4,7 @@
 // 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
 
 
-const mainUsersDiv = document.getElementById('users')
+const mainUsersDiv = document.getElementsByClassName('users')[0];
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => value.json())
